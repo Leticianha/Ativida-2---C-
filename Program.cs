0a720a03d3126cd134f165a4179d3f3e83e80326
Leticia Cristina <133662502+Leticianha@users.noreply.github.com>
2024-05-08 16:08:54 -0300

    .

---
+++ Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Net;
+
+namespace SegundoProjeto
+{
+    class Projeto
+    {
+        static void Main(String[] args)
+        {
+            /*ATIVIDADE 1*/
+
+            /*1 – Uma empresa vende sorvetes no valor atacado, se o cliente
+            comprar até 25 unidades, é cobrado R$ 1.50 por unidade, acima disso,
+            é cobrado R$ 1.25.Faça um código que calcule quanto um
+            determinado cliente irá pagar. Para isso, declare uma variável que
+            armazenará a quantidade de sorvete(você pode escolher a
+            quantidade)*/
+
+            /*int quantidade = 10;
+
+
+           if (quantidade <= 25) {
+                 float total = 1.50f * quantidade;
+                Console.WriteLine("Será cobrado " +  total);
+            } else {
+                float total = 1.25f * quantidade;
+                Console.WriteLine("Será cobrado R$" + total);
+            }*/
+
+            /*ATIVIDADE 2*/
+
+            /*2 – Declare uma variável com um número, se ele for maior que 10, faça
+            o cálculo do número + 10 * 2. Senão, faça número + 9 * 3 – 8. (Você pode
+            escolher o número da variável)*/
+
+            /*int numero = 32;
+
+            if (numero > 10) {
+                int total = (numero + 10) * 2;
+                Console.WriteLine(total);
+            } else
+            {
+                int total = (numero + 9) * 3 - 8;
+                Console.WriteLine(total);
+            }*/
+
+            /*ATIVIDADE 3*/
+
+            /*3 – Faça um código que verifique se um número é positivo ou
+            negativo (você pode escolher o número da variável)*/
+
+            /* int numero = 37;
+
+             if (numero > 0 ) {
+                 Console.WriteLine("Número positivo");
+             } else if (numero < 0 ) {
+                 Console.WriteLine("Número negativo");
+             } else
+             {
+                 Console.WriteLine("Número neutro = 0");
+             }*/
+
+            /*ATIVIDADE 4*/
+
+            /*4 – Faça um código que tenha duas variáveis numéricas e mostre qual
+            delas é a maior (você pode escolher os números das variáveis)*/
+
+            /*int num1 = 15;
+            int num2 = 14;
+
+            if (num1 > num2)
+            {
+                Console.WriteLine(num1 + " é maior que " + num2);
+            }
+            else
+            {
+                Console.WriteLine(num2 + " é maior que " + num1);
+            }*/
+
+            /*ATIVIDADE 5*/
+
+            /*5 – Um professor deseja um sistema para o cálculo da média dos seus
+            alunos, para isso, declare duas variáveis para as notas, realize o cálculo
+            da média e mostre, se a média for maior ou igual a 7, mostre
+            “Aprovado”, senão mostre “Reprovado” (você pode escolher os
+            números das variáveis)*/
+
+            /*float nota1 = 8.5f;
+            int nota2 = 9;
+
+            double media = (nota1 + nota2) / 2;
+
+            if (media >= 7) {
+                Console.WriteLine("Aprovado");
+            } else
+            {
+                Console.WriteLine("Reprovado");
+            }*/
+
+        }
+    }
+}
